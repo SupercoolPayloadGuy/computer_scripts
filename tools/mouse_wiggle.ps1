d8a0d47f34a12f92c55ff680cfa1253ab72be944
SupercoolPayloadGuy <techandcodetester@gmail.com>
2026-03-27 12:20:41 +0100

    Update mouse_wiggle.ps1

--- tools/mouse_wiggle.ps1
+++ tools/mouse_wiggle.ps1
@@ -1,8 +1,13 @@
-Add-Type -AssemblyName System.Windows.Forms
+# mouse_wiggle.ps1
+if ([Environment]::UserInteractive) {
+    Add-Type -AssemblyName System.Windows.Forms
 
-for ($i = 0; $i -lt 20; $i++) {
-    $pos = [System.Windows.Forms.Cursor]::Position
-    [System.Windows.Forms.Cursor]::Position = New-Object Drawing.Point($pos.X + 12, $pos.Y)
-    Start-Sleep -Milliseconds 100
-    [System.Windows.Forms.Cursor]::Position = $pos
+    for ($i = 0; $i -lt 20; $i++) {
+        $pos = [System.Windows.Forms.Cursor]::Position
+        [System.Windows.Forms.Cursor]::Position = New-Object Drawing.Point($pos.X + 12, $pos.Y)
+        Start-Sleep -Milliseconds 100
+        [System.Windows.Forms.Cursor]::Position = $pos
+    }
+} else {
+    Write-Host "Mouse wiggle requires an interactive desktop."
 }
