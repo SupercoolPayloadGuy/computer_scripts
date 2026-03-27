5b7937275ba286287fef11acf5ced951845ab45f
SupercoolPayloadGuy <techandcodetester@gmail.com>
2026-03-27 11:58:14 +0100

    Create matrix.ps1

---
+++ tools/matrix.ps1
@@ -0,0 +1,24 @@
+Add-Type -AssemblyName System.Windows.Forms
+
+$form = New-Object Windows.Forms.Form
+$form.Text = "System Activity"
+$form.Width = 600
+$form.Height = 400
+
+$box = New-Object Windows.Forms.RichTextBox
+$box.Dock = "Fill"
+$box.BackColor = "Black"
+$box.ForeColor = "Lime"
+$box.Font = "Consolas, 10"
+$form.Controls.Add($box)
+
+$form.Show()
+
+for ($i = 0; $i -lt 200; $i++) {
+    $line = -join ((33..126) | Get-Random -Count 60 | ForEach-Object {[char]$_})
+    $box.AppendText("$line`n")
+    Start-Sleep -Milliseconds 50
+}
+
+Start-Sleep 2
+$form.Close()
